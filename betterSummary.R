0f127957dc63ad244bb919ea60d5eb193a8ef802
roman-schuster <schuster@sas.upenn.edu>
2016-03-15 12:08:54 -0400

    Update betterSummary.R
    Added Breusch-Pagan-Godfrey test

--- betterSummary.R
+++ betterSummary.R
@@ -1,8 +1,13 @@
-# This file contains statistical functions for use in econometrics (econ104)
+# This file contains statistical functions and a summary function
+# For use in econometrics (econ104) at the University of Pennsylvania
 # Created by Roman Schuster on 3/13/16
 
-# Creating a function that takes in a linear model and returns the sum of squared
-# residuals from that model
+# Required dependencies:
+library(lmtest) # for bptest() function
+library(stats) # for logLik() function
+
+# Creating a function that takes in a linear model and
+# returns the sum of squared residuals from that model
 ssr <- function(lm) {
   # Very straightforward, literally summing the squared residuals from the
   # 'residuals' attribute of the linear model
@@ -11,28 +16,29 @@ ssr <- function(lm) {
   return(output)
 }
 
-# Creating a function that takes in a data set, linear model of the data and number
-# of regressors and returns the standard error of regression
-ser <- function(lm, data, numRegressors) {
+# Creating a function that takes in a data set and a linear model 
+# of the data and returns the standard error of regression
+ser <- function(data, lm) {
   # Obtaining the sum of squared residuals from the linear model using ssr()
   ssrFromData <- ssr(lm)
   # Grabbing the sample size from the data using nrow()
   sampleSize <- nrow(data)
+  # Grabbing the number of regression coefficients with length()
+  numRegressors <- length(lm$coefficients)
   
   # Following the formula SER = sqrt(SSR/(N - K))
   return(sqrt(ssrFromData/(sampleSize - numRegressors)))
 }
 
-# R already has the logLik function that returns the log liklihood for a given
-# linear model, so I'll use that to make functions for the SIC and AIC
-
-# Creating a function that takes in the data, linear model of the data and number
-# of regressors (including intercept) and returns the Schwarz Information Criterion:
-sic <- function(data, lm, numRegressors) {
+# Creating a function that takes in a data set and a linear model 
+# of the data and returns the Schwarz Information Criterion:
+sic <- function(data, lm) {
   # Using the logLik() function to get the log likelihood of the linear model
   logLikFromLm <- logLik(lm)
   # Grabbing the sample size from the data using the nrow() function
   sampleSize <- nrow(data)
+  # Grabbing the number of regression coefficients with length()
+  numRegressors <- length(lm$coefficients)
   
   # Following the formula:
   # SIC = -2*L/T + (K*Ln(T))/T
@@ -45,13 +51,15 @@ sic <- function(data, lm, numRegressors) {
   return(output)
 }
 
-# Creating a function that takes in the data, linear model of the data and number
-# of regressors (including intercept) and returns the Akaike Information Criterion
-aic <- function(data, lm, numRegressors) {
+# Creating a function that takes in a data set and a linear  
+# model of the data and returns the Akaike Information Criterion:
+aic <- function(data, lm) {
   # Using the logLik() function to get the log likelihood of the linear model
   logLikFromLm <- logLik(lm)
   # Grabbing the sample size from the data using the nrow() function
   sampleSize <- nrow(data)
+  # Grabbing the number of regression coefficients with length()
+  numRegressors <- length(lm$coefficients)
   
   # Following the formula:
   # AIC = -2*L/T + 2*K/T
@@ -64,13 +72,15 @@ aic <- function(data, lm, numRegressors) {
   return(output)
 }
 
-# Creating a function that takes in the data, a lienar model of the data, and the
-# number of regressors (including intercept) and returns the Hannan-Quinn Criterion
-hq <- function(data, lm, numRegressors) {
+# Creating a function that takes in a data set and a linear 
+# model of the data and returns the Hannan-Quinn Criterion
+hq <- function(data, lm) {
   # Using the logLik() function to get the log likelihood of the linear model
   logLikFromLm <- logLik(lm)
   # Grabbing the sample size from the data using the nrow() function
   sampleSize <- nrow(data)
+  # Grabbing the number of regression coefficients with length()
+  numRegressors <- length(lm$coefficients)
   
   # Following the formula:
   # HQ = -2*(L/T) + 2*K*Ln(Ln(T))/T
@@ -83,8 +93,8 @@ hq <- function(data, lm, numRegressors) {
   return(output)
 }
 
-# Creating a function that takes in a data set and the linear model of the data set
-# and returns the Durbin-Watson Statistic
+# Creating a function that takes in a data set and a linear 
+# model of the data set and returns the Durbin-Watson Statistic
 dw <- function(data, lm) {
   # Grabbing the sum of squared residuals from the linear model using ssr()
   ssrFromData <- ssr(lm)
@@ -106,28 +116,62 @@ dw <- function(data, lm) {
   return(output)
 }
 
-# Rolling a summary function that takes in a data set, a linear approximation of
- # the data set, and the number of regressors (including intercept) used in the
- # linear model and returns relevant statistical data
-betterSummary <- function(data, lm, numRegressors) {
-  # Printing out the coefficient estimates, std. errors, t-values and p-values
-  # (from the coefficients attribute of the summary function)
+# Creating a function that takes in a data set and 
+# a linear model of the data set and returns
+# a comprehensive statistical analysis
+statSummary <- function(data, lm) {
+  # Printing out the coefficient summary, includes:
+  # Coefficient estimate
+  # Std. error of coefficient estimate
+  # T-Statistic of coefficient estimate
+  # P-Value of T-Statistic for each coefficient estimate
+  # MARK: Uses the summary function from the stats package
   print(summary(lm)$coefficients)
   
-  # Calling on prior functions to obtain the relevant statistical data
+  # Grabbing the number of regression coefficients using length()
+  numRegressors <- length(lm$coefficients)
+  
+  # Calling functions from this file, as well as the stats
+  # and lmtest packages, to obtain relevant statistical data
+  # and put them into variables to print out at the end
+  
+  # Sum of Squared Residuals
   ssrFromData <- ssr(lm)
+  # Log Likelihood
   logLikFromData <- logLik(lm)
-  aicFromData <- aic(data, lm, numRegressors)
-  sicFromData <- sic(data, lm, numRegressors)
-  hqFromData <- hq(data, lm, numRegressors)
+  
+  # Akaike Information Criterion
+  aicFromData <- aic(data, lm)
+  # Schwarz Information Criterion
+  sicFromData <- sic(data, lm)
+  # Hannan-Quinn Criterion
+  hqFromData <- hq(data, lm)
+  
+  # Durbin-Watson Statistic
   dwFromData <- dw(data, lm)
+  
+  # R Squared
   rSquaredFromData <- summary(lm)$r.squared
+  # Adjusted R Squared
   adjRSquaredFromData <- summary(lm)$adj.r.squared
-  serFromData <- ser(lm, data, numRegressors)
+  
+  # Std. Error of Regression
+  serFromData <- ser(data, lm)
+  # F-Statistic of the regression
   fStatArrayFromData <- summary(lm)$fstatistic
+  # Grabbing just the F-Stat from the data to avoid printing
+  # the number of degrees of freedom along with it
   fStatFromData <- fStatArrayFromData[1]
+  # Calculating the p-value for the f-statistic
   pValueFStat <- 1 - pf(fStatFromData, fStatArrayFromData[2], fStatArrayFromData[3])
-
+  
+  # Breusch-Pagan-Godfrey Test for Heteroskedasticity
+  breuschPaganGodfrey <- bptest(lm, data = data)
+  # Grabbing just the BGP stat (not degrees of freedom)
+  bpStat <- breuschPaganGodfrey$statistic
+  # Grabbing the p-value of the BGP
+  bpPValue <- breuschPaganGodfrey$p.value
+  
   # Printing out the data in a clean, easy to read format
   cat('\nR Squared: ',substitute(rSquaredFromData),'\n')
   cat('Adjusted R Squared: ', substitute(adjRSquaredFromData), '\n')
@@ -143,4 +187,12 @@ betterSummary <- function(data, lm, numRegressors) {
   cat('Schwarz Info. Criterion:  ',substitute(sicFromData),'\n')
   cat('Hannan-Quinn Criterion: ', substitute(hqFromData), '\n')
   cat('Durbin-Watson Statistic: ', substitute(dwFromData), '\n')
+  
+  cat('\nBreusch-Pagan-Godfrey Test [N*(R^2)]: ', substitute(bpStat), '\n')
+  cat('BPG Test [p-value]: ', substitute(bpPValue), '\n')
 }
+
+
+
+
+
